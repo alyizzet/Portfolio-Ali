959da14a9ad410e4b863486cd96721d6a40c384c
Ali İzzet Metin <izzet.metin@triodor.eu>
2020-06-03 17:41:09 +0300

    Updates

---
+++ precache-manifest.2a7c2ed7d1a86cdc04317e91b2eb0cdf.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "548b7afab9f30f1025ee7ebed82a68e4",
+    "url": "/portAli/home/index.html"
+  },
+  {
+    "revision": "fa826b24d5061e24258e",
+    "url": "/portAli/home/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "a33b912f1ef538ebdcf3",
+    "url": "/portAli/home/static/css/main.3f8b9011.chunk.css"
+  },
+  {
+    "revision": "fa826b24d5061e24258e",
+    "url": "/portAli/home/static/js/2.e405aab9.chunk.js"
+  },
+  {
+    "revision": "a33b912f1ef538ebdcf3",
+    "url": "/portAli/home/static/js/main.1532614d.chunk.js"
+  },
+  {
+    "revision": "3f55b7e0d0083b072dce",
+    "url": "/portAli/home/static/js/runtime~main.103c76d4.js"
+  },
+  {
+    "revision": "fd38e7b9de48c577ef150b48aad4e6b7",
+    "url": "/portAli/home/static/media/bullsvsbears.fd38e7b9.jpg"
+  },
+  {
+    "revision": "bf79b6757027c409ee6c6107ee2074ce",
+    "url": "/portAli/home/static/media/devgrub.bf79b675.png"
+  },
+  {
+    "revision": "3da49c686de364f2e513abcafa25f3ca",
+    "url": "/portAli/home/static/media/evverest.3da49c68.png"
+  },
+  {
+    "revision": "1e7e03c438e15ae534b3ed111e3da8ec",
+    "url": "/portAli/home/static/media/linkedin.1e7e03c4.png"
+  },
+  {
+    "revision": "190313a5a34aca869b1befbe5481c8ca",
+    "url": "/portAli/home/static/media/lstm.190313a5.png"
+  }
+]);
